1415906b5e45fdeb2b7eb7654b671b1508b1424c
Sai Krishna Goka <gskr1803@gmail.com>
2026-08-22 07:26:06 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,3 @@
+  let root = ReactDOM.createRoot(document.getElementById("root"))
+    let reactelement = React.createElement('h2', {id:"child"}, 'hello I am from react');
+    root.render(reactelement);
