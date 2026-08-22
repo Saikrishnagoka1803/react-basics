bb7b960409fe4fa286a0e79b88c50e0a36f1abab
Sai Krishna Goka <gskr1803@gmail.com>
2026-08-22 21:26:59 +0100

    igniting the app parcel suoerpowers

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-  let root = ReactDOM.createRoot(document.getElementById("root"))
-    let reactelement = React.createElement('h2', {id:"child"}, 'hello I am from react');
-    root.render(reactelement);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const reactelement = React.createElement('h2', {id:"child"}, 'hello I am from react emoji 🚀');
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(reactelement);
