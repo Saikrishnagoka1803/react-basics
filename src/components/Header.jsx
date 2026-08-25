a3e5944f04a4171a5009e94b3199a9d882bd31d1
Sai Krishna Goka <gskr1803@gmail.com>
2026-08-25 11:54:35 +0100

    imp of link and avoid a tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,32 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const Header = () => (
-    <div id="header">
-        <div id="logo">
-            <img
-                src={
-                    "https://s3.amazonaws.com/images.seroundtable.com/google-paint-brusges-1531914263.jpg"
-                }
-            ></img>
+const Header = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <div id="header">
+            <div id="logo">
+                <img
+                    src={
+                        "https://s3.amazonaws.com/images.seroundtable.com/google-paint-brusges-1531914263.jpg"
+                    }
+                ></img>
+            </div>
+            <div id="nav-items">
+                <ul className="nav-items-list">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About us</Link></li>
+                    <li><Link to="/contact">contact</Link></li>
+                    <li onClick={() => setIsLoggedIn(!isLoggedIn)}>
+                        {isLoggedIn ? "Logout" : "Login"}
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div id="nav-items">
-            <ul className="nav-items-list">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About us</a></li>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/contact">contact</a></li>
-            </ul>
-        </div>
-    </div>
-);
+    );
+
+}
 
 export default Header;
