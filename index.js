32612aabc1fe2c21de88a610f18b6913be18ac7e
Sai Krishna Goka <gskr1803@gmail.com>
2026-08-25 11:06:33 +0100

    router fixes with versioning

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Contact from "./src/components/Contact.jsx";
 import About from "./src/components/About.jsx";
+import Error from "./src/components/Error.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -12,14 +13,17 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <Error />,
   },
   {
     path: "/contact",
     element: <Contact />,
+    errorElement: <Error />,
   },
   {
     path: "/about",
     element: <About />,
+    errorElement: <Error />,
   },
 ]);
 
---
+++ src/components/Error.jsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+
+    const error = useRouteError();
+    console.log(error);
+
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <h2>{error.error.message}</h2>
+        </div>
+    );
+};
+
+export default Error;
