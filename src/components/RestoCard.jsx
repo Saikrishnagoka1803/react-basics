c5c7151f412972f84575f47a851a74078ca2571b
Sai Krishna Goka <gskr1803@gmail.com>
2026-08-25 12:12:47 +0100

    fixing image issue

--- src/components/RestoCard.jsx
+++ src/components/RestoCard.jsx
@@ -1,12 +1,16 @@
-import { GET_RESTO_IMG } from './utils/constants.js'
+import { CARD_IMAGE_URL, fixNoImage } from './utils/constants.js'
 
 const RestoCard = ({ restaurantInfo }) => {
     return (
         <div className="card">
             <img
                 className="card-img-top"
-                src={GET_RESTO_IMG + restaurantInfo.cloudinaryImageId}
+                src={fixNoImage(restaurantInfo.cloudinaryImageId)}
                 alt="Card image cap"
+                onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = CARD_IMAGE_URL;
+                }}
             />
             <div className="card-body">
                 <h5 className="card-title">{restaurantInfo.name}</h5>
@@ -17,7 +21,7 @@ const RestoCard = ({ restaurantInfo }) => {
                     {restaurantInfo.cuisines.join(", ")}
                 </p>
                 <p className="card-text">
-                     {restaurantInfo.totalRatingsString}
+                    {restaurantInfo.totalRatingsString}
                 </p>
                 <a href="#" className="btn btn-primary">
                     {restaurantInfo.avgRating} stars,
--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -1,3 +1,10 @@
 export const CARD_IMAGE_URL = "https://picsum.photos/300/200";
 
-export const GET_RESTO_IMG = `https://media-assets.swiggy.com/swiggy/image/upload/`;
+const GET_RESTO_IMG = `https://media-assets.swiggy.com/swiggy/image/upload/`;
+
+export const fixNoImage = (imgId) => {
+  if (imgId === null) {
+    return CARD_IMAGE_URL;
+  }
+  return GET_RESTO_IMG + imgId;
+};
